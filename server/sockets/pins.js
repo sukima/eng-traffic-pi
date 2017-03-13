fabaf1cc92db97e0077044fedf91c24503745068
Devin Weaver <suki@tritarget.org>
2017-03-12 23:21:21 -0400

    Update server mocks for new group option

--- server/sockets/pins.js
+++ server/sockets/pins.js
@@ -6,6 +6,7 @@ var greenLight = new GPIOPin(17, {
   name: 'Green Light',
   color: '#2ecc71',
   order: 30,
+  group: 'traffic',
   description: 'Come on in and hang out. If the door is closed feel free to open it.'
 });
 
@@ -13,6 +14,7 @@ var yellowLight = new GPIOPin(18, {
   name: 'Yellow Light',
   color: '#f1c40f',
   order: 20,
+  group: 'traffic',
   description: 'Hard at work focusing. Inturupt if it is urgent or extreamly quick.'
 });
 
@@ -20,13 +22,32 @@ var redLight = new GPIOPin(27, {
   name: 'Red Light',
   color: '#e74c3c',
   order: 10,
+  group: 'traffic',
   description: 'In a meeting; do not disturb. Inturupt only if the site is down.'
+});
+
+var warningLight = new GPIOPin(28, {
+  name: 'Warning Light',
+  color: '#e5348a',
+  order: 40,
+  group: null,
+  description: 'A simple warning light'
+});
+
+var infoLight = new GPIOPin(29, {
+  name: 'Information Light',
+  color: '#bada55',
+  order: 50,
+  group: null,
+  description: 'A simple informational light'
 });
 
 var gpioPins = new Map([
   [17, greenLight],
   [18, yellowLight],
-  [27, redLight]
+  [27, redLight],
+  [28, warningLight],
+  [29, infoLight]
 ]);
 
 module.exports = function(io) {
