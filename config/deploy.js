f29568f744b2a28138ff5ce6966546a8daf4eaf9
Devin Weaver <suki@tritarget.org>
2017-03-13 13:50:05 -0400

    Use the real ember-cli-deploy

--- config/deploy.js
+++ config/deploy.js
@@ -1,12 +1,33 @@
-module.exports = function(environment){
-  var ENV = {};
+/* jshint node: true */
 
-  if (environment === 'production') {
+module.exports = function(deployTarget) {
+  var ENV = {
+    build: {}
+    // include other plugin configuration that applies to all deploy targets here
+  };
+
+  if (deployTarget === 'development') {
+    ENV.build.environment = 'development';
+    // configure other plugins for development deploy target here
+  }
+
+  if (deployTarget === 'staging') {
+    ENV.build.environment = 'production';
+    // configure other plugins for staging deploy target here
+  }
+
+  if (deployTarget === 'production') {
+    ENV.build.environment = 'production';
+    // configure other plugins for production deploy target here
     ENV['scp'] = {
       username: 'pi',
       host: '10.0.0.100',
       path: '/var/www/html'
     }
-  };
+  }
+
+  // Note: if you need to build some configuration asynchronously, you can return
+  // a promise that resolves with the ENV object instead of returning the
+  // ENV object synchronously.
   return ENV;
 };
