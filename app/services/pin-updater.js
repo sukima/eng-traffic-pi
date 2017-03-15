70fee9a5f36a71c866c84a78cf392b2f67a79d66
Devin Weaver <suki@tritarget.org>
2017-03-15 15:36:17 -0400

    Make refresh polling constant
    Seems the listen on write event is not broadcast. We are forced to do
    polling without changing the server code.

--- app/services/pin-updater.js
+++ app/services/pin-updater.js
@@ -9,40 +9,23 @@ const {
 const POLL_TIME = 1000;
 
 export default Service.extend(Evented, {
-  io: service(),
   store: service(),
 
-  _lastPoll: 0,
   _pollingEnabled: false,
 
   backgroundRefresh() {
-    if (!get(this, '_pollingEnabled')) {
-      return;
-    }
-    let now = new Date().getTime();
-    if (now - get(this, '_lastPoll') > POLL_TIME * 2) {
+    if (get(this, '_pollingEnabled')) {
       get(this, 'store').findAll('pin').catch(() => {});
+      later(this, 'backgroundRefresh', POLL_TIME);
     }
-    set(this, '_lastPoll', now);
-    later(this, 'backgroundRefresh', POLL_TIME);
-  },
-
-  onUpdate(data) {
-    const store = get(this, 'store');
-    store.pushPayload('pin', data);
-    this.trigger('update', data);
   },
 
   start() {
-    const socket = get(this, 'io.socket');
-    socket.on('pin:write', this.onUpdate, this);
     set(this, '_pollingEnabled', true);
     this.backgroundRefresh();
   },
 
   stop() {
-    const socket = get(this, 'io.socket');
     set(this, '_pollingEnabled', false);
-    socket.off('pin:write', this.onUpdate);
   }
 });
