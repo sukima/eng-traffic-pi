057ba484252ee7dd5360669c65c7fb184dad34da
Devin Weaver <suki@tritarget.org>
2017-03-15 16:17:23 -0400

    Notify user when pins change in background

--- app/services/pin-updater.js
+++ app/services/pin-updater.js
@@ -8,6 +8,16 @@ const {
 
 const POLL_TIME = 1000;
 
+function arraysEqual(a, b) {
+  if (a === b) { return true; }
+  if (a == null || b == null) { return false; }
+  if (a.length !== b.length) { return false; }
+  for (let i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) { return false; }
+  }
+  return true;
+}
+
 export default Service.extend(Evented, {
   store: service(),
 
@@ -15,9 +25,23 @@ export default Service.extend(Evented, {
 
   backgroundRefresh() {
     if (get(this, '_pollingEnabled')) {
-      get(this, 'store').findAll('pin').catch(() => {});
+      this.refresh().catch(() => {});
       later(this, 'backgroundRefresh', POLL_TIME);
     }
+  },
+
+  refresh() {
+    return get(this, 'store').findAll('pin')
+      .then(records => this.notifyChanges(records));
+  },
+
+  notifyChanges(records) {
+    let newPinState = records.mapBy('enabled');
+    let oldPinState = get(this, '_pinState') || newPinState;
+    if (!arraysEqual(newPinState, oldPinState)) {
+      this.trigger('update');
+    }
+    set(this, '_pinState', newPinState);
   },
 
   start() {
